fca998388db09b77b9c796b682c2cecf743abfdd
Mark Packham <markpackham1@gmail.com>
2018-01-04 21:41:25 +0000

    first commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string str = "true";
+                bool b = Convert.ToBoolean(str);
+                Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("This could not be converted");
+            }
+                     
+        }
+    }
+}
