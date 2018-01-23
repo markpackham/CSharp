6128b067dbc9a99f94daec194ac727ba3fe09342
Mark Packham <markpackham1@gmail.com>
2018-01-23 17:47:05 +0000

    Learn about Arrays from https://www.youtube.com/watch?v=bBG2o905sRQ&feature=youtu.be&t=10m3s

---
+++ LoopingArraysStringBuilder/LoopingArraysStringBuilder/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace LoopingArraysStringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] favNums = new int[3];
+            favNums[0] = 23;
+
+            Console.WriteLine("favNum 0 : {0}", favNums[0]);
+
+            string[] customer = { "Bob", "Sally", "Sue" };
+
+            var employees = new[] { "Mike", "Paul", "Rick" };
+
+            object[] randomArray = { "Paul", 45, 1.234 };
+
+            Console.WriteLine("randomArray 0 : {0}", randomArray[2].GetType());
+
+            Console.WriteLine("Array Size : {0}", randomArray.Length);
+
+            for(int i = 0; i <randomArray.Length; i++)
+            {
+                Console.WriteLine("Array Size {0} : Value : {1}", i, randomArray[i]);
+            }
+
+            string[,] custNames = new string[2, 2] { { "Bob", "Smith" }, { "Sally", "Marks" } };
+
+            Console.WriteLine("MD Value : {0}", custNames.GetValue(1,1));
+
+            for(int i = 0; i< custNames.GetLength(0); i++)
+            {
+                for (int j = 0; j < custNames.GetLength(1); j++)
+                {
+                    Console.Write("{0}", custNames[i,j]);
+                }
+                Console.WriteLine();
+            }
+
+            int[] randNums = { 1, 4, 9, 2 };
+
+            randNums.SetValue(9000, 2);
+
+            Console.WriteLine("1 at Index : {0}", Array.IndexOf(randNums,1));
+
+            Array.Reverse(randNums);
+
+            Array.Sort(randNums);
+
+            printArray(randNums, "ForEach");
+
+            Console.ReadLine();
+        }
+
+        static void printArray(int[] intArray, string mess)
+        {
+            foreach(int k in intArray)
+            {
+                Console.WriteLine("{0} : {1}", mess, k);
+            }
+        }
+    }
+}
