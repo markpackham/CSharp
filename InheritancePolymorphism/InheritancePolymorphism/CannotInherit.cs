a1a4f1bfd91bfc97240f25005d6e6686fa9547c8
Mark Packham <markpackham1@gmail.com>
2018-01-26 16:37:27 +0000

    Add sealed class like Java's final class

---
+++ InheritancePolymorphism/InheritancePolymorphism/CannotInherit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritancePolymorphism
+{
+    sealed class CannotInherit
+    {
+        //A sealed class is one you can't inherit from like a "final" class in Java
+    }
+}
