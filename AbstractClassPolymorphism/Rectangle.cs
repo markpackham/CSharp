559898f77dd4267719835b198d3b04f22ef598c7
Mark Packham <markpackham1@gmail.com>
2018-01-26 19:31:18 +0000

    Revise Abstract Class Polymorphism from https://www.youtube.com/watch?v=RaHm_pSxqLY

---
+++ AbstractClassPolymorphism/Rectangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AbstractClassPolymorphism
+{
+    class Rectangle : Shape
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+
+        public Rectangle(double length,
+            double width)
+        {
+            Name = "Rectangle";
+            Length = length;
+            Width = width;
+        }
+
+        public override double Area()
+        {
+            return Length * Width;
+        }
+
+        // You can replace the method using override
+        public override void GetInfo()
+        {
+            // Execute the base version
+            base.GetInfo();
+            Console.WriteLine($"It has a Length of {Length} and Width of {Width}");
+        }
+    }
+}
