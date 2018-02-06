6dd57a853a63e78f06910c3cf05af79e6ea15c38
Mark Packham <markpackham1@gmail.com>
2018-02-06 18:00:02 +0000

    WPF Xaml Layout types learnt from https://www.youtube.com/watch?v=G-BRSwClK_4&list=PLGLfVvz_LVvRX6xK1oi0reKci6ignjdSa&index=19

---
+++ WPFStackPanel/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StackPanel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string usersName = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // You can set window properties in code
+            this.Title = "Hello World";
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        // When the button is clicked it closes the window
+        private void Button1Clicked(object sender, RoutedEventArgs e)
+        {
+            // Opens a message box
+            MessageBox.Show("The App is Closing");
+
+            // Closes the app
+            this.Close();
+        }
+
+        // Event handling that posts the mouse x, y position in title
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            Title = e.GetPosition(this).ToString();
+        }
+
+        private void BtnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            // Opens an open file dialog
+            OpenFileDialog openDlg = new OpenFileDialog();
+            openDlg.ShowDialog();
+        }
+
+        private void BtnSaveFile_Click(object sender, RoutedEventArgs e)
+        {
+            // Opens a save file dialog
+            SaveFileDialog saveDlg = new SaveFileDialog();
+            saveDlg.ShowDialog();
+        }
+
+        private void Send_Button_Click(object sender, RoutedEventArgs e)
+        {
+            //usersName = UsersName.Text;
+
+            //MessageBox.Show($"Hello {usersName}");
+        }
+    }
+}
---
+++ WPFWrapPanel/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WrapPanel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string usersName = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Event handling that posts the mouse x, y position in title
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            Title = e.GetPosition(this).ToString();
+        }
+
+        private void Send_Button_Click(object sender, RoutedEventArgs e)
+        {
+            usersName = UsersName.Text;
+
+            MessageBox.Show($"Hello {usersName}");
+        }
+    }
+}
