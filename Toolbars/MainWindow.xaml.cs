0b11b31a617f5e313b80baf68bfb696309a58fb8
Mark Packham <markpackham1@gmail.com>
2018-02-08 09:09:51 +0000

    Learn of Toolbars from https://www.youtube.com/watch?v=HEzMn2mvsW8&index=21&list=PLGLfVvz_LVvRX6xK1oi0reKci6ignjdSa

---
+++ Toolbars/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Toolbars
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MenuOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openDlg = new OpenFileDialog();
+            openDlg.ShowDialog();
+        }
+
+        private void MenuSave_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveDlg = new SaveFileDialog();
+            saveDlg.ShowDialog();
+        }
+
+        private void MenuFontTimes_Click(object sender, RoutedEventArgs e)
+        {
+            menuFontCourier.IsChecked = false;
+            menuFontArial.IsChecked = false;
+            txtBoxDoc.FontFamily = new FontFamily("Times New Roman");
+        }
+
+        private void MenuFontCourier_Click(object sender, RoutedEventArgs e)
+        {
+            menuFontTimes.IsChecked = false;
+            menuFontArial.IsChecked = false;
+            txtBoxDoc.FontFamily = new FontFamily("Courier");
+        }
+
+        private void MenuFontArial_Click(object sender, RoutedEventArgs e)
+        {
+            menuFontCourier.IsChecked = false;
+            menuFontTimes.IsChecked = false;
+            txtBoxDoc.FontFamily = new FontFamily("Arial");
+        }
+
+        // ---------- NEW STUFF ----------
+
+        // Used to track if font size combobox is closed
+        private bool comboFSClosed = true;
+
+        // Change font size of text box after combo is closed
+        private void ComboFontSize_DropDownClosed(object sender, EventArgs e)
+        {
+            if (comboFSClosed) ChangeTBFontSize();
+            comboFSClosed = true;
+        }
+
+        // Verify combo is closed and call for font size change
+        private void ComboFontSize_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox combobox = sender as ComboBox;
+            comboFSClosed = !combobox.IsDropDownOpen;
+            ChangeTBFontSize();
+        }
+
+        private void ChangeTBFontSize()
+        {
+            // Convert combo font size data to string
+            string fontsize = comboFontSize.SelectedItem.ToString();
+
+            // Get the last 2 numbers
+            fontsize = fontsize.Substring(fontsize.Length - 2);
+
+            switch (fontsize)
+            {
+                case "10":
+                    txtBoxDoc.FontSize = 10;
+                    break;
+                case "12":
+                    txtBoxDoc.FontSize = 12;
+                    break;
+                case "14":
+                    txtBoxDoc.FontSize = 14;
+                    break;
+                case "16":
+                    txtBoxDoc.FontSize = 16;
+                    break;
+            }
+
+        }
+    }
+}
