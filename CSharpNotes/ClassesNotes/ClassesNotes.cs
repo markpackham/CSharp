917a61e905aa48a190d7d9ca3aa89b872b00f987
Mark Packham <markpackham1@gmail.com>
2019-08-07 16:33:17 +0100

    Class & Static notes learned from Codecadmey, this stuff is very common in other languages

---
+++ CSharpNotes/ClassesNotes/ClassesNotes.cs
@@ -0,0 +1,10 @@
+/*
+Instantiate an object using new
+Define fields, the pieces of data for each class
+Define properties, the spokespeople for each field
+Define automatic properties, the shorthand for making properties
+Define methods, the actions a class can take
+Define constructors, the special methods called when a class is instantiated
+Overload constructors and reuse code with this
+Control access to class members using public and private
+*/
